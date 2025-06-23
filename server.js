256d0be6ff788512bbf15fdcca8b1e7868e84825
ᴛᴀᴋᴀʜɪʀᴏᴇx666 <105994154+TakahiroEX666@users.noreply.github.com>
2025-06-23 17:40:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ export default {
 
         const filename = `${Date.now()}_${file.name}`;
 
-        await env.MY_BUCKET.put(filename, file.stream(), {
+        await uploads.put(filename, file.stream(), {
           httpMetadata: {
             contentType: file.type,
           },
